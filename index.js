2da8f1755da63ac76d5082ddab4f4ac348a95580
Samuel Brasil <samuel.brasil@gmail.com>
2019-04-21 18:21:17 -0300

    Criado a branch development, o arquivo package.json, index.js e instalado o modulo npm readline-sync

---
+++ index.js
@@ -0,0 +1,22 @@
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a Wikipedia search term:')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is','What is','The history of']
+        const selectPrefixIndex = readline.keyInSelect(prefixes)
+        const selectPrefixText = prefixes[selectPrefixIndex]
+
+        return selectPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
